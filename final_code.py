c9f36c0c69e55d81ca15661700328ecb7dbc8be1
jsp1ll23 <js773@evansville.edu>
2018-04-11 15:06:04 -0900

    All effects under same file

--- final_code.py
+++ final_code.py
@@ -30,50 +30,82 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 #user definitions
 
 def send2Pd1(message=' '):
-    os.system("echo '" + message + "' |pdsend 3400")
+    os.system("echo '" + message + "' |pdsend 3500")
 
 def send2Pd2(message=' '):
-    os.system("echo '" + message + "' |pdsend 3300")
+    os.system("echo '" + message + "' |pdsend 3400")
 
 def send2Pd3(message=' '):
+    os.system("echo '" + message + "' |pdsend 3300")
+
+def send2Pd4(message=' '):
     os.system("echo '" + message + "' |pdsend 3200")
+
+def send2Pd5(message=' '):
+    os.system("echo '" + message + "' |pdsend 3100")
 
 def AudioOff():
     message = '0 0;'
-    send2Pd1(message)
+    send2Pd2(message)
 
 def AudioOn():
     message = '0 1;'
-    send2Pd1(message)
+    send2Pd2(message)
 
 def setMix1(mix1):
     message = '0 ' + str(mix1) + ';'
-    send2Pd2(message)
-
-def setMix2(mix2):
-    message = '0 ' + str(mix2) + ';'
     send2Pd3(message)
 
-def RunEffect(x):
-    subprocess.Popen(['nohup', 'pd','/home/pi/Senior_Project/'+ x + '.pd'],
-                 stdout=open('/dev/null', 'w'),
-                 stderr=open('logfil.log', 'a'),
-                 preexec_fn=os.setpgrp)
+def setMix2(mix2):
+    message = '1 ' + str(mix2) + ';'
+    send2Pd3(message)
 
-def StopEffect():
-    os.system('pkill pd')
+def setroom(mix1):
+    message = '0 ' + str(mix1) + ';'
+    send2Pd5(message)
 
+def setdamp(mix2):
+    message = '1 ' + str(mix2) + ';'
+    send2Pd5(message)
+
+def setWet():
+    message = '0 0.5;'
+    send2Pd4(message)
+
+def setDry():
+    message = '1 0.5;'
+    send2Pd4(message)
+
+def Clean(x):
+    message = '0 ' +str(x) + ';'
+    send2Pd1(message)
+
+def Distortion(x):
+    message = '1 ' +str(x) + ';'
+    send2Pd1(message)
+
+def Reverb(x):
+    message = '2 ' +str(x) + ';'
+    send2Pd1(message)
 
 def getmix1_distortion(x):
-    value = (x/1023)*200
+    value = (x/1023)*1
     return value
 
 def getmix2_distortion(x):
-    value = (x/1023)*100
+    value = (x/1023)*50
     return value
 
-lcd.clear()
+def getmix_reverb(x):
+    value = (x/1023)
+    return value
+
 Old_Filename = '0'
+lcd.clear()
+subprocess.Popen(['nohup', 'pd','/home/pi/final_pd.pd'],
+            stdout=open('/dev/null', 'w'),
+            stderr=open('logfil.log', 'a'),
+            preexec_fn=os.setpgrp)
 
 while True:
     values = [0]*3
@@ -83,26 +115,35 @@ while True:
     Selector = values[2]
     Mix1_ADC = values[1]
     Mix2_ADC = values[0]
-    if(Selector < 512 ):
+    if(Selector < 342 ):
         Filename = 'Clean'
         Mixname = '\nNon-edit'
         Mix1 = 0
         Mix2 = 0
-    else:
+        Reverb(0)
+        Distortion(0)
+        Clean(1)
+    elif(Selector > 341 and Selector < 683):
         Filename = 'Distortion'
         Mixname = '\nHarshness, Vol'
-        Mix2 = getmix2_distortion(float(Mix2_ADC))
-        Mix1 = getmix1_distortion(float(Mix1_ADC))
+        setMix1(getmix1_distortion(float(Mix1_ADC)))
+        setMix2(getmix2_distortion(float(Mix2_ADC)))
+        Clean(0)
+        Reverb(0)
+        Distortion(1)
+    else:
+        Filename = 'Reverb'
+        Mixname = '\nLength, Damping'
+        setroom(getmix_reverb(float(Mix1_ADC)))
+        setdamp(getmix_reverb(float(Mix2_ADC)))
+        setWet()
+        setDry()
+        Clean(0)
+        Distortion(0)
+        Reverb(1)
     if(Old_Filename != Filename):
-        StopEffect()
         lcd.clear()
-        RunEffect(Filename)
         lcd.message(Filename)
         lcd.message(Mixname)
     AudioOn()
-    time.sleep(.2)
-    setMix1(Mix1)
-    time.sleep(.2)
-    setMix2(Mix2)
-    time.sleep(.2)
     Old_Filename = Filename
